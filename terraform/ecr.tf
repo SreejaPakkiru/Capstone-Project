35a0976cd10a0f86900c53ecf4797b518521a325
Sreeja Pakkiru <Sreeja.Pakkiru@WinWire.com>
2025-12-04 10:17:43 +0530

    new commit

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "capstone-repo" {
-  name                 = "capstone-app"
+  name                 = "capstone-repo"
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
