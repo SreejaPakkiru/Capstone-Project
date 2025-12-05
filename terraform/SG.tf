c906c12b0f94f92cefffbb29d37d5ebd7b4a165e
SreejaPakkiru <Sreeja.Pakkiru@WinWire.com>
2025-12-05 10:54:42 +0530

    Update SG.tf

--- terraform/SG.tf
+++ terraform/SG.tf
@@ -43,22 +43,3 @@ resource "aws_security_group" "SG" {
   }
 }
 
-resource "aws_security_group" "alb_sg" {
-  name        = "alb-sg"
-  description = "Allow HTTP to ALB"
-  vpc_id      = aws_vpc.VPC.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
